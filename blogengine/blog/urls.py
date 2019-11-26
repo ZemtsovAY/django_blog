0e23d340c2ef731ede34ec5a258ef591885babfe
ZemtsovAY <3emtsovAY@gmail.com>
2019-11-26 08:24:21 +0400

    add __init__.py

--- blogengine/blog/urls.py
+++ blogengine/blog/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('post/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', TagList.as_view(), name='tag_list_url'),
-    path('tag/create', TagCreate.as_view(), name='tag_create_url'),
-    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url')
+    path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
+    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url')
+
 ]
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.views.generic import View
 
@@ -38,20 +39,29 @@ class TagCreate(ObjectCreateMixin, View):
     form = TagForm
     template = 'blog/tag_create.html'
 
-    # def get(self, request):
-    #     form = TagForm()
-    #     return render(request, 'blog/tag_create.html', context={'form': form})
-    #
-    # def post(self, request):
-    #     bound_form = TagForm(request.POST)
-    #
-    #     if bound_form.is_valid():
-    #         new_tag = bound_form.save()
-    #         return redirect(new_tag)
-    #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
-
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+
+class TagUpdate(View):
+    def get(self, request, slug):
+        # tag = Tag.objects.get(slug__iexact=slug)
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        bound_form = TagForm(instance=tag)
+        print(slug)
+        return render(request, 'blog/tag_update.html', context={'form': bound_form, 'tag': tag})
+
+    def post(self, request, slug):
+        tag = get_object_or_404(Tag, slug__iexact=slug)
+        bound_form = TagForm(request.POST, instance=tag)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_update.html', context={'form': bound_form, 'tag': tag})
+
+
+
 
